26360047ecc923419a9c71ced10f422206a7a179
albertgoncalves <goncalves.albert@gmail.com>
2019-01-09 00:02:37 -0500

    Cleaner.

--- shell.sh
+++ shell.sh
@@ -2,14 +2,12 @@
 
 set -e
 
-os=$(uname -s)
-
 cache_dir="lib"
 if [ ! -e ./$cache_dir/ ]; then
     mkdir $cache_dir
 fi
 
-if [ $os = "Darwin" ]; then
+if [ $(uname -s) = "Darwin" ]; then
     nix-shell src/darwin.nix
 else
     nix-shell src/linux.nix
