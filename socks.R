90301ebbcee871b6d2260e21f53d5196038ff14f
albertgoncalves <goncalves.albert@gmail.com>
2019-01-12 01:56:39 -0500

    Pointless.

--- socks.R
+++ socks.R
@@ -6,13 +6,16 @@ sourceCpp("socks.cpp", cacheDir="lib")
 # via http://www.sumsar.net/blog/2014/10/tiny-data-and-the-socks-of-karl-broman/
 
 main = function() {
+    set.seed(1)
     y = rep(1, 11)
     n = 1000000
 
     sims = n_sims(n, y)
 
-    if (length(sims) > 0) {
+    len = length(sims)
+    if (len > 0) {
         hist(sims)
+        print(len)
     }
 }
 
